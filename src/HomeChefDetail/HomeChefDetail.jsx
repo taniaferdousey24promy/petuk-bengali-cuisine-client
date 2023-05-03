d79b0c1c78ffe8bf4374d2a9ec335b3ffd79f2e6
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-05-04 05:47:18 +0600

    the home page with six cards and each of them showing the information about chef pic,chef name,years of experience,numbers of recipe ,likes and view recipes button

--- src/HomeChefDetail/HomeChefDetail.jsx
+++ src/HomeChefDetail/HomeChefDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, Col, Row } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const HomeChefDetail = () => {
@@ -18,14 +18,40 @@ const HomeChefDetail = () => {
         {chefs.map((chef) => (
           <Col key={chef.id}>
             <Card>
-              <Card.Img variant="top" src={chef.chief_photo} />
+              <Card.Img variant="top" className="rounded-circle mx-auto mt-5" src={chef.chief_photo} style={{height:'200px',width:'200px'}} />
               <Card.Body>
-                <Card.Title>{chef.name}</Card.Title>
-                <Card.Text>
+                <Card.Title className="text-center fs-1" >{chef.name}</Card.Title>
+
+                <div className=" mt-3 d-flex justify-content-center">
+
+                <Button  >View Recipe</Button>
+
+
+                </div>
+                <Card.Text className="mt-4" >
                   This is a longer card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
                   longer.
                 </Card.Text>
+
+                <div className="d-flex justify-content-between text-center" >
+                    <div className=" ps-5 pe-5 border-end fs-5 ">
+                        <p>Likes</p>
+                        <p className="fw-bold">{chef.number_of_likes}</p>
+                    </div>
+
+                    <div className=" pe-5 border-end  fs-5 ">
+                        <p>Number Of Recipes</p>
+                        <p className="fw-bold">{chef.number_of_recipes}</p>
+
+                    </div>
+
+                    <div className="ps-5 pe-5  fs-5 ">
+                        <p>Experience</p>
+                        <p className="fw-bold">{chef.years_of_experience} Years</p>
+
+                    </div>
+                </div>
               </Card.Body>
             </Card>
           </Col>
